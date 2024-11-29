4edd57c2cb4bfdd3bfb3198f01c19fe1f887f483
zX3no <28522740+zX3no@users.noreply.github.com>
2024-11-29 16:03:11 +1030

    Greatly improved horizontal and vertical centering flexbox

--- src/lib.rs
+++ src/lib.rs
@@ -109,10 +109,17 @@ macro_rules! tlayout {
     (($widget:expr),*) => {};
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Center {
+    Horizontal,
+    Vertical,
+    Both,
+}
+
 //This does centers all widgets horizontally.
 #[macro_export]
-macro_rules! layout {
-    ($vw:expr, $vh:expr, $($widget:expr),*) => {{
+macro_rules! flex_center {
+    ($center:expr, $vw:expr, $vh:expr, $($widget:expr),*) => {{
         // let mut widgets = Vec::new();
 
         // $(
@@ -122,51 +129,40 @@ macro_rules! layout {
 
         let mut test = Vec::new();
 
+        let center: Center = $center;
         let mut segments: Vec<Segment> = Vec::new();
+
         let viewport_width: usize = $vw;
         let viewport_height: usize = $vh;
+
         let mut total_width = 0;
+        let mut total_height = 0;
+
         let mut max_width = 0;
+        let mut max_height = 0;
 
         //The total height of largest widget in each segment.
         let mut total_height_of_largest = 0;
-        let mut total_hsegments = 0;
+        let mut total_width_of_largest = 0;
 
-        let mut max_height = 0;
         // let horizontal_wrap = 0;
         // let vertical_wrap = 0;
 
-        // let count = widgets.len();
-        let count = count_expr!($($widget),*);
+        const COUNT: usize = const { count_expr!($($widget),*) };
+        // let seg = [const { Segment::new() }; COUNT];
         let mut i = 0;
 
         let mut widget_count = 0;
 
+        //The first loop is required to calculate the segments.
         $(
             let area = $widget.area();
 
             i += 1;
-
-            //Skip the zero width segment.
-            //This is pretty much a hack and should be removed in the third re-write.
-            if total_width + area.width > viewport_width && !(total_width == 0 || max_width == 0) {
-                segments.push(Segment {
-                    direction: Direction::Horizontal,
-                    size: total_width,
-                    max: max_width,
-                    widget_count,
-                });
-
-                total_hsegments += 1;
-                total_height_of_largest += max_height;
-                max_height = 0;
-                total_width = 0;
-                widget_count = 0;
-                max_width = 0;
-            }
+            widget_count += 1;
 
             total_width += area.width;
-            // total_height += area.height;
+            total_height += area.height;
 
             if area.width > max_width {
                 max_width = area.width;
@@ -176,59 +172,93 @@ macro_rules! layout {
                 max_height = area.height;
             }
 
-            //TODO: Could just have a start and end index into widgets
-            //This would be faster and less stupid.
-            // segment_widgets.push((area, primative));
-            widget_count += 1;
+            match center {
+                Center::Horizontal => {
+                    if (total_width + area.width > viewport_width) || i == COUNT {
+                        segments.push(Segment {
+                            direction: Direction::Horizontal,
+                            size: total_width,
+                            max: max_width,
+                            widget_count,
+                        });
 
-            //Don't like this part.
-            if (i == count) {
-                total_hsegments += 1;
-                total_height_of_largest += max_height;
-                segments.push(Segment {
-                    direction: Direction::Horizontal,
-                    size: total_width,
-                    max: max_width,
-                    widget_count,
-                });
-                widget_count = 0;
-            }
+                        total_height_of_largest += max_height;
+
+                        max_height = 0;
+                        max_width = 0;
+
+                        total_width = 0;
+                        widget_count = 0;
+                    }
+                }
+                Center::Vertical => {
+                    if (total_height + area.height > viewport_height) || i == COUNT  {
+                        segments.push(Segment {
+                            direction: Direction::Vertical,
+                            size: total_height,
+                            max: max_height,
+                            widget_count,
+                        });
+
+                        total_width_of_largest += max_width;
+
+                        max_height = 0;
+                        max_width = 0;
+
+                        total_height = 0;
+                        widget_count = 0;
+                    }
+                },
+                Center::Both => {
+                }
+            };
         )*
 
-        // dbg!(&segments);
-        let vspacing =
-            viewport_height.saturating_sub(total_height_of_largest) / (total_hsegments + 1);
-        let mut x = 0;
-        let mut y = vspacing;
-        let mut wid = 0;
-        let mut seg = 0;
-        let mut spacing = 0;
-        let mut max_height = 0;
+        let vspacing = viewport_height.saturating_sub(total_height_of_largest) / segments.len();
+        let hspacing = viewport_width.saturating_sub(total_width_of_largest) / segments.len();
+        // dbg!(vspacing, hspacing, viewport_width, total_width_of_largest, total_height_of_largest, &segments);
+
+        let (mut x, mut y, mut spacing) = match center {
+            Center::Horizontal => {
+                let spacing = viewport_width.saturating_sub(segments[0].size) / (segments[0].widget_count + 1);
+                (spacing, 0, spacing)
+            },
+            Center::Vertical => {
+                let spacing = viewport_height.saturating_sub(segments[0].size) / (segments[0].widget_count + 1);
+                (0, spacing, spacing)
+            },
+            Center::Both => todo!(),
+        };
+
+        let mut widget_index = 0;
+        let mut segment_index = 0;
 
         $(
-            let mut segment = &segments[seg];
-            if wid == 0 {
-                spacing = viewport_width.saturating_sub(segment.size) / (segment.widget_count + 1);
-                x = spacing;
-            }
+            let mut segment = &segments[segment_index];
 
-            if wid >= segment.widget_count {
-                wid = 0;
-                seg += 1;
-                y += max_height + vspacing;
+            if widget_index >= segment.widget_count {
+                widget_index = 0;
+                segment_index += 1;
+                segment = &segments[segment_index];
 
-                segment = &segments[seg];
-                spacing = viewport_width.saturating_sub(segment.size) / (segment.widget_count + 1);
-                x = spacing;
+                match center {
+                    Center::Horizontal => {
+                        spacing = viewport_width.saturating_sub(segment.size) / (segment.widget_count + 1);
+                        x = spacing;
+                        y += segment.max + vspacing;
+                    },
+                    Center::Vertical => {
+                        spacing = viewport_height.saturating_sub(segment.size) / (segment.widget_count + 1);
+                        x += segment.max + hspacing;
+                        y = spacing;
+                    },
+                    Center::Both => todo!(),
+                };
             }
 
             let w = widget(&mut $widget);
 
-            let area = w.area_mut().unwrap();
-
-            if area.height > max_height {
-                max_height = area.height;
-            }
+            let area = w.area_mut();
 
             area.x = x;
             area.y = y;
@@ -241,9 +271,14 @@ macro_rules! layout {
 
             //This is where the draw call would typically be issued.
             test.push((area, w.primative()));
-            x += spacing + area.width;
 
-            wid += 1;
+            match center {
+                Center::Horizontal => x += spacing + area.width,
+                Center::Vertical => y += spacing + area.height,
+                Center::Both => todo!(),
+            };
+
+            widget_index += 1;
         )*
 
         test
@@ -382,33 +417,6 @@ macro_rules! flex {
     }};
 }
 
-// let temp = flex(10, 10, &widgets);
-//Not sure how to call click here.
-//There is a new area that is created and not set to the original widget despite
-//being passed in with a mutable reference.
-//That would mean you would need to call an on click function that takes in
-//the original widget `&mut $widget` and a new calculated area. I think
-//currently the area is gotten from the widget itself with widget.area().
-//```rs
-//if clicked(ctx, &mut self.widget, self.click.$idx.0) {
-//    self.click.$idx.1(&mut self.widget);
-//}
-//```
-//clicked takes in &mut self.widget then calls `let area = *self.area_mut().unwrap();`
-//To fix this I guess I'd need change try_click to be, fn try_click(&mut self, area: Rect);
-//Then
-//```rs
-//if clicked(ctx, area, self.click.$idx.0) {
-//    self.click.$idx.1(&mut self.widget);
-//}
-//```
-//do all the layout calculations and figue out where everything should go.
-
-//I'm not sure if it's possible to calculate the widgets with a single step of the macro.
-//If we assume that everything that is passed in can be a reference to something. that should make copy pasting easier.
-
-//once the widget has the correct position, run the click function.
-
 #[derive(Debug)]
 pub struct Segment {
     pub direction: Direction,
@@ -417,121 +425,17 @@ pub struct Segment {
     pub size: usize,
     ///Max width or max height
     pub max: usize,
-    // pub widgets: Vec<(Rect, Primative)>,
     pub widget_count: usize,
 }
-
-#[derive(Debug)]
-pub struct OldSegment {
-    pub direction: Direction,
-    ///Either the total height or width.
-    ///Depends on the direction.
-    pub size: usize,
-    ///Max width or max height
-    pub max: usize,
-    pub widgets: Vec<(Rect, Primative)>,
-}
-
-pub fn flex(
-    viewport_width: usize,
-    viewport_height: usize,
-    widgets: &[(Rect, Primative)],
-) -> Vec<(Rect, Primative)> {
-    let mut temp_primatives = Vec::new();
-    let mut segments: Vec<OldSegment> = Vec::new();
-    // let viewport_width = ctx().area.width;
-    // let viewport_height = ctx().area.height;
-    let mut total_width = 0;
-    let mut max_width = 0;
-
-    //The total height of largest widget in each segment.
-    let mut total_height_of_largest = 0;
-    let mut total_hsegments = 0;
-
-    let mut max_height = 0;
-    // let horizontal_wrap = 0;
-    // let vertical_wrap = 0;
-
-    let mut segment_widgets = Vec::new();
-    let count = widgets.len();
-    let mut i = 0;
-
-    for (area, primative) in widgets {
-        i += 1;
-
-        //Skip the zero width segment.
-        //This is pretty much a hack and should be removed in the third re-write.
-        if total_width + area.width > viewport_width && !(total_width == 0 || max_width == 0) {
-            segments.push(OldSegment {
-                direction: Direction::Horizontal,
-                size: total_width,
-                max: max_width,
-                widgets: core::mem::take(&mut segment_widgets),
-            });
-
-            total_hsegments += 1;
-            total_height_of_largest += max_height;
-            max_height = 0;
-            total_width = 0;
-            max_width = 0;
-        }
-
-        total_width += area.width;
-        // total_height += area.height;
-
-        if area.width > max_width {
-            max_width = area.width;
-        }
-
-        if area.height > max_height {
-            max_height = area.height;
-        }
-
-        //TODO: Could just have a start and end index into widgets
-        //This would be faster and less stupid.
-        segment_widgets.push((*area, primative.clone()));
-
-        //Don't like this part.
-        if i == count {
-            total_hsegments += 1;
-            total_height_of_largest += max_height;
-            segments.push(OldSegment {
-                direction: Direction::Horizontal,
-                size: total_width,
-                max: max_width,
-                widgets: core::mem::take(&mut segment_widgets),
-            })
+impl Segment {
+    pub const fn new() -> Self {
+        Self {
+            direction: Direction::Horizontal,
+            size: 0,
+            max: 0,
+            widget_count: 0,
         }
     }
-
-    let vspacing = viewport_height.saturating_sub(total_height_of_largest) / (total_hsegments + 1);
-    let mut y = vspacing;
-
-    for segment in segments {
-        let spacing = viewport_width.saturating_sub(segment.size) / (segment.widgets.len() + 1);
-        let mut x = spacing;
-        let mut max_height = 0;
-
-        match segment.direction {
-            Direction::Horizontal => {
-                for (mut area, primative) in segment.widgets {
-                    if area.height > max_height {
-                        max_height = area.height;
-                    }
-
-                    area.x = x;
-                    area.y = y;
-                    // unsafe { COMMAND_QUEUE.push(Primative { area, primative }) };
-                    temp_primatives.push((area, primative));
-                    x += spacing + area.width;
-                }
-                y += max_height + vspacing;
-            }
-            Direction::Vertical => {}
-        }
-    }
-
-    temp_primatives
 }
 
 #[cfg(test)]
@@ -540,7 +444,7 @@ mod tests {
 
     #[test]
     fn flex_horizontal() {
-        let mut h = Header {
+        let mut h1 = Header {
             title: "hi",
             area: Rect {
                 x: 0,
@@ -574,7 +478,7 @@ mod tests {
         let vw = 50;
         let vh = 40;
 
-        let test = flex!(Flex::TopLeft, Direction::Horizontal, vw, vh, h, h2, h3);
+        let test = flex!(Flex::TopLeft, Direction::Horizontal, vw, vh, h1, h2, h3);
 
         assert_eq!(test[0].0.x, 0);
         assert_eq!(test[0].0.y, 0);
@@ -585,7 +489,7 @@ mod tests {
         assert_eq!(test[2].0.x, 0);
         assert_eq!(test[2].0.y, 20);
 
-        let test = flex!(Flex::TopRight, Direction::Horizontal, vw, vh, h, h2, h3);
+        let test = flex!(Flex::TopRight, Direction::Horizontal, vw, vh, h1, h2, h3);
         assert_eq!(test[0].0.x, 50);
         assert_eq!(test[0].0.y, 0);
 
@@ -595,7 +499,7 @@ mod tests {
         assert_eq!(test[2].0.x, 50);
         assert_eq!(test[2].0.y, 20);
 
-        let test = flex!(Flex::BottomLeft, Direction::Horizontal, vw, vh, h, h2, h3);
+        let test = flex!(Flex::BottomLeft, Direction::Horizontal, vw, vh, h1, h2, h3);
 
         assert_eq!(test[0].0.x, 0);
         assert_eq!(test[0].0.y, 40);
@@ -606,7 +510,7 @@ mod tests {
         assert_eq!(test[2].0.x, 0);
         assert_eq!(test[2].0.y, 20);
 
-        let test = flex!(Flex::BottomRight, Direction::Horizontal, vw, vh, h, h2, h3);
+        let test = flex!(Flex::BottomRight, Direction::Horizontal, vw, vh, h1, h2, h3);
 
         assert_eq!(test[0].0.x, 50);
         assert_eq!(test[0].0.y, 40);
@@ -616,11 +520,23 @@ mod tests {
 
         assert_eq!(test[2].0.x, 50);
         assert_eq!(test[2].0.y, 20);
+
+        let vw = 70;
+        let vh = 40;
+
+        let test = flex_center!(Center::Horizontal, vw, vh, h1, h2);
+        assert_eq!(test.len(), 2);
+
+        assert_eq!(test[0].0.x, 10);
+        assert_eq!(test[0].0.y, 0);
+
+        assert_eq!(test[1].0.x, 40);
+        assert_eq!(test[1].0.y, 0);
     }
 
     #[test]
     fn flex_vertical() {
-        let mut h = Header {
+        let mut h1 = Header {
             title: "hi",
             area: Rect {
                 x: 0,
@@ -654,7 +570,7 @@ mod tests {
         let vw = 50;
         let vh = 50;
 
-        let test = flex!(Flex::TopLeft, Direction::Vertical, vw, vh, h, h2, h3);
+        let test = flex!(Flex::TopLeft, Direction::Vertical, vw, vh, h1, h2, h3);
 
         assert_eq!(test[0].0.x, 0);
         assert_eq!(test[0].0.y, 0);
@@ -665,7 +581,7 @@ mod tests {
         assert_eq!(test[2].0.x, 20);
         assert_eq!(test[2].0.y, 0);
 
-        let test = flex!(Flex::TopRight, Direction::Vertical, vw, vh, h, h2, h3);
+        let test = flex!(Flex::TopRight, Direction::Vertical, vw, vh, h1, h2, h3);
 
         assert_eq!(test[0].0.x, 50);
         assert_eq!(test[0].0.y, 0);
@@ -676,7 +592,7 @@ mod tests {
         assert_eq!(test[2].0.x, 30);
         assert_eq!(test[2].0.y, 0);
 
-        let test = flex!(Flex::BottomLeft, Direction::Vertical, vw, vh, h, h2, h3);
+        let test = flex!(Flex::BottomLeft, Direction::Vertical, vw, vh, h1, h2, h3);
 
         assert_eq!(test[0].0.x, 0);
         assert_eq!(test[0].0.y, 50);
@@ -687,7 +603,7 @@ mod tests {
         assert_eq!(test[2].0.x, 20);
         assert_eq!(test[2].0.y, 50);
 
-        let test = flex!(Flex::BottomRight, Direction::Vertical, vw, vh, h, h2, h3);
+        let test = flex!(Flex::BottomRight, Direction::Vertical, vw, vh, h1, h2, h3);
 
         assert_eq!(test[0].0.x, 50);
         assert_eq!(test[0].0.y, 50);
@@ -704,7 +620,7 @@ mod tests {
         let vw = 40;
         let vh = 40;
 
-        let mut header = Header {
+        let mut h1 = Header {
             title: "hi",
             area: Rect {
                 x: 0,
@@ -714,7 +630,7 @@ mod tests {
             },
         };
 
-        let mut header2 = Header {
+        let mut h2 = Header {
             title: "hi",
             area: Rect {
                 x: 0,
@@ -724,7 +640,7 @@ mod tests {
             },
         };
 
-        let test = layout!(vw, vh, header, header2);
+        let test = flex_center!(Center::Horizontal, vw, vh, h1, h2);
         assert_eq!(test.len(), 2);
         assert_eq!(test[0].0.x, 0);
         assert_eq!(test[1].0.x, 20);
@@ -739,7 +655,7 @@ mod tests {
             },
         };
 
-        let test = layout!(vw, vh, header, header2, header3);
+        let test = flex_center!(Center::Horizontal, vw, vh, h1, h2, header3);
         assert_eq!(test.len(), 3);
         assert_eq!(test[0].0.x, 0);
         assert_eq!(test[1].0.x, 20);
@@ -756,12 +672,57 @@ mod tests {
             },
         };
 
-        let test = layout!(vw, vh, header, header2, header3, header4);
+        let test = flex_center!(Center::Horizontal, vw, vh, h1, h2, header3, header4);
 
         assert_eq!(test.len(), 4);
         assert_eq!(test[0].0.x, 0);
         assert_eq!(test[1].0.x, 20);
         assert_eq!(test[2].0.x, 0);
         assert_eq!(test[3].0.x, 20);
+    }
+
+    #[test]
+    fn vcenter() {
+        let vw = 40;
+        let vh = 40;
+
+        let mut h1 = Header {
+            title: "hi",
+            area: Rect { x: 0, y: 0, width: 20, height: 20 },
+        };
+
+        let mut h2 = Header {
+            title: "hi",
+            area: Rect { x: 0, y: 0, width: 20, height: 20 },
+        };
+
+        let mut h3 = Header {
+            title: "hi",
+            area: Rect { x: 0, y: 0, width: 20, height: 20 },
+        };
+
+        let test = flex_center!(Center::Vertical, vw, vh, h1, h2, h3);
+        assert_eq!(test.len(), 3);
+
+        assert_eq!(test[0].0.x, 0);
+        assert_eq!(test[0].0.y, 0);
+
+        assert_eq!(test[1].0.x, 0);
+        assert_eq!(test[1].0.y, 20);
+
+        assert_eq!(test[2].0.x, 20);
+        assert_eq!(test[2].0.y, 10);
+
+        let vw = 40;
+        let vh = 70;
+
+        let test = flex_center!(Center::Vertical, vw, vh, h1, h2);
+        assert_eq!(test.len(), 2);
+
+        assert_eq!(test[0].0.x, 0);
+        assert_eq!(test[0].0.y, 10);
+
+        assert_eq!(test[1].0.x, 0);
+        assert_eq!(test[1].0.y, 40);
     }
 }
