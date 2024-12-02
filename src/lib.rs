01ff22157c4d136683c71169efabccb9c21b42ee
zX3no <28522740+zX3no@users.noreply.github.com>
2024-12-02 15:33:56 +1030

    centered flex functions

--- src/lib.rs
+++ src/lib.rs
@@ -147,6 +147,7 @@ pub struct Flex<F: DrawFlex> {
 }
 
 impl<F: DrawFlex> Flex<F> {
+    //TODO: Not sure how to do this better.
     pub fn force_draw(&mut self) {
         let mut f = self.f.take();
         if let Some(f) = &mut f {
@@ -183,6 +184,154 @@ impl<F: DrawFlex> std::fmt::Debug for Flex<F> {
     }
 }
 
+pub fn calculate_segments<T: Widget>(
+    center: Center,
+    segments: &mut Vec<Segment>,
+    widget: &mut T,
+    count: usize,
+    viewport_width: usize,
+    viewport_height: usize,
+    mut i: usize,
+    mut widget_count: usize,
+    mut max_width: usize,
+    mut max_height: usize,
+    mut total_width: usize,
+    mut total_height: usize,
+    mut total_width_of_largest: usize,
+    mut total_height_of_largest: usize,
+) -> (usize, usize, usize, usize, usize, usize, usize, usize) {
+    let area = widget.area();
+
+    i += 1;
+    widget_count += 1;
+
+    total_width += area.width;
+    total_height += area.height;
+
+    if area.width > max_width {
+        max_width = area.width;
+    }
+
+    if area.height > max_height {
+        max_height = area.height;
+    }
+
+    match center {
+        Center::Horizontal => {
+            if (total_width + area.width > viewport_width) || i == count {
+                segments.push(Segment {
+                    direction: Direction::Horizontal,
+                    size: total_width,
+                    max: max_width,
+                    widget_count,
+                });
+
+                total_height_of_largest += max_height;
+
+                max_height = 0;
+                max_width = 0;
+
+                total_width = 0;
+                widget_count = 0;
+            }
+        }
+        Center::Vertical => {
+            if (total_height + area.height > viewport_height) || i == count {
+                segments.push(Segment {
+                    direction: Direction::Vertical,
+                    size: total_height,
+                    max: max_height,
+                    widget_count,
+                });
+
+                total_width_of_largest += max_width;
+
+                max_height = 0;
+                max_width = 0;
+
+                total_height = 0;
+                widget_count = 0;
+            }
+        }
+        Center::Both => {
+            todo!()
+        }
+    };
+
+    (
+        i,
+        widget_count,
+        max_width,
+        max_height,
+        total_width,
+        total_height,
+        total_width_of_largest,
+        total_height_of_largest,
+    )
+}
+
+pub fn draw_segments<T: Widget>(
+    widget: &mut T,
+    center: Center,
+    segments: &[Segment],
+    mut widget_index: usize,
+    mut segment_index: usize,
+    mut x: usize,
+    mut y: usize,
+    viewport_width: usize,
+    viewport_height: usize,
+    mut spacing: usize,
+    vspacing: usize,
+    hspacing: usize,
+    temp: &mut Vec<(Rect, Primative)>,
+) -> (usize, usize, usize, usize, usize) {
+    let mut segment = &segments[segment_index];
+
+    if widget_index >= segment.widget_count {
+        widget_index = 0;
+        segment_index += 1;
+        segment = &segments[segment_index];
+
+        match center {
+            Center::Horizontal => {
+                spacing = viewport_width.saturating_sub(segment.size) / (segment.widget_count + 1);
+                x = spacing;
+                y += segment.max + vspacing;
+            }
+            Center::Vertical => {
+                spacing = viewport_height.saturating_sub(segment.size) / (segment.widget_count + 1);
+                x += segment.max + hspacing;
+                y = spacing;
+            }
+            Center::Both => todo!(),
+        };
+    }
+
+    let area = widget.area_mut().unwrap();
+
+    area.x = x;
+    area.y = y;
+
+    //Stop the mutable borrow.
+    let area = widget.area();
+
+    //Click the widget once the layout is calculated.
+    widget.try_click();
+
+    //This is where the draw call would typically be issued.
+    temp.push((area, widget.primative()));
+
+    match center {
+        Center::Horizontal => x += spacing + area.width,
+        Center::Vertical => y += spacing + area.height,
+        Center::Both => todo!(),
+    };
+
+    widget_index += 1;
+
+    (widget_index, segment_index, x, y, spacing)
+}
+
 #[macro_export]
 macro_rules! f {
     ($($widget:expr),*) => {{
@@ -205,8 +354,88 @@ macro_rules! f {
                         let (x, y, max_width, max_height) = flex_basic(direction,quadrant, w, x, y, start_x, start_y, max_height, max_width, viewport_width, viewport_height, &mut temp);
                     )*
                 }
-                FlexMode::Center(_) => {
-                    todo!();
+                FlexMode::Center(center) => {
+                    let mut segments: Vec<Segment> = Vec::new();
+
+                    let total_width = 0;
+                    let total_height = 0;
+                    let max_width = 0;
+                    let max_height = 0;
+                    //The total height of largest widget in each segment.
+                    let total_height_of_largest = 0;
+                    let total_width_of_largest = 0;
+                    let i = 0;
+                    let widget_count = 0;
+
+                    const COUNT: usize = const { count_expr!($($widget),*) };
+
+                    //The first loop is required to calculate the segments.
+                    $(
+                        let w = widget(&mut $widget);
+
+                        #[allow(unused)]
+                        let (i, widget_count, max_width, max_height, total_width, total_height, total_width_of_largest, total_height_of_largest) = calculate_segments(
+                            center,
+                            &mut segments,
+                            w,
+                            COUNT,
+                            viewport_width,
+                            viewport_height,
+                            i,
+                            widget_count,
+                            max_width,
+                            max_height,
+                            total_width,
+                            total_height,
+                            total_width_of_largest,
+                            total_height_of_largest,
+                        );
+                    )*
+
+                    //This is named poorly, I honestly can't even remember what this is for...
+                    let vspacing = viewport_height.saturating_sub(total_height_of_largest) / segments.len();
+                    let hspacing = viewport_width.saturating_sub(total_width_of_largest) / segments.len();
+
+                    //Spacing is really the segment spacing.
+                    let (x, y, spacing) = match center {
+                        Center::Horizontal => {
+                            let spacing = viewport_width.saturating_sub(segments[0].size) / (segments[0].widget_count + 1);
+                            (spacing, 0, spacing)
+                        },
+                        Center::Vertical => {
+                            let spacing = viewport_height.saturating_sub(segments[0].size) / (segments[0].widget_count + 1);
+                            (0, spacing, spacing)
+                        },
+                        Center::Both => {
+                            let x = viewport_width.saturating_sub(segments[0].size) / (segments[0].widget_count + 1);
+                            let y = viewport_height.saturating_sub(segments[0].size) / (segments[0].widget_count + 1);
+                            //I think I'll need to keep both types of segment spacing.
+                            (x, y, 0)
+                        },
+                    };
+
+                    let widget_index = 0;
+                    let segment_index = 0;
+
+                    $(
+                        let w = widget(&mut $widget);
+                        #[allow(unused)]
+                        let (widget_index, segment_index, x, y, spacing) = draw_segments(
+                            w,
+                            center,
+                            &segments,
+                            widget_index,
+                            segment_index,
+                            x,
+                            y,
+                            viewport_width,
+                            viewport_height,
+                            spacing,
+                            vspacing,
+                            hspacing,
+                            &mut temp,
+                        );
+                    )*
                 }
             }
 
@@ -309,6 +538,7 @@ macro_rules! flex_center {
                             max: max_height,
                             widget_count,
                         });
+                //TODO: Cleanup and move into functions.
 
                         total_width_of_largest += max_width;
 
