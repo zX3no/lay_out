3939ed759d51497b6ca193304749eaf04bcfc4b6
Bay <28522740+zX3no@users.noreply.github.com>
2025-03-12 18:57:26 +1030

    testing recursion

--- examples/arena.rs
+++ examples/arena.rs
@@ -46,6 +46,7 @@ struct Rect {
     pub width: usize,
     pub height: usize,
 }
+
 impl Add for Rect {
     type Output = Self;
 
@@ -93,6 +94,8 @@ impl Rect {
 
 //I'll keep it simple for now,
 //there are likely ways to improve this.
+//TODO: This should be a small vec
+//1000 items would be 16KB (128 * 1000 / 8 / 1000 = 16)
 #[derive(Default, Debug)]
 struct Arena(Vec<Node>);
 
@@ -138,6 +141,8 @@ impl Arena {
     }
 }
 
+//It is important that nodes are as small as possible
+//Currently they are 128bits
 #[derive(Default, Debug, Clone)]
 struct Node {
     area: Rect,
@@ -146,11 +151,71 @@ struct Node {
     gap: usize,
     index: usize,
     parent: Option<usize>,
+    //TODO: This should be a small vec.
     children: Vec<usize>,
+}
+
+//This is probably less optimal than using reccursion.
+//I'm guessing because of the extra allocations.
+struct DfsIter<'a> {
+    arena: &'a Arena,
+    stack: Vec<usize>,
+}
+
+impl<'a> DfsIter<'a> {
+    fn new(arena: &'a Arena, root: usize) -> Self {
+        Self {
+            arena,
+            stack: vec![root],
+        }
+    }
+}
+
+impl<'a> Iterator for DfsIter<'a> {
+    type Item = &'a Node;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let node_idx = self.stack.pop()?;
+        let node = self.arena.get_node(node_idx)?;
+        self.stack.extend(node.children.iter().rev());
+        Some(node)
+    }
+}
+
+//https://stackoverflow.com/questions/65948553/why-is-recursion-not-suggested-in-rust
+//Tail call elimination is missed when calling this, I have checked the assembly it uses `call example::traverse::h58697ea0ab44a8ac`
+fn traverse(arena: &Arena, node_idx: usize) {
+    if let Some(node) = arena.get_node(node_idx) {
+        println!("Visiting node: {:?}", node);
+
+        for &child_idx in &node.children {
+            traverse(arena, child_idx);
+        }
+    }
+}
+
+fn node_test() {
+    let mut ar = Arena::default();
+
+    let parent = ar.create_node(None);
+
+    let mut red = Rect::new(0, 0, 300, 300).into_node();
+    let mut yellow = Rect::new(0, 0, 350, 200).into_node();
+
+    let red = ar.add_node(red, Some(parent));
+    let yellow = ar.add_node(yellow, Some(parent));
+    ar.add_node(Rect::new(0, 0, 20, 20).into_node(), Some(red));
+
+    // for node in DfsIter::new(&ar, parent) {
+    //     println!("{:?}", node);
+    // }
+
+    traverse(&ar, parent);
 }
 
 fn main() {
     defer_results!();
+    return node_test();
 
     let mut ar = Arena::default();
 
@@ -165,7 +230,7 @@ fn main() {
     //If we want to avoid the clone here,
     //we need to guarentee that `ar[parent_idx].children` is not changed while iterating.
     for child_idx in ar[parent].children.clone() {
-        let [root, child] = ar.get_disjoint_mut([parent, child_idx]).unwrap();
+        let [root, child] = unsafe { ar.get_disjoint_unchecked_mut([parent, child_idx]) };
 
         root.area.width += root.padding.left + root.padding.right;
         root.area.height += root.padding.top + root.padding.bottom;
